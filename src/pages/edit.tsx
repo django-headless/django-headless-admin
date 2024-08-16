5b780334eaf5927f17669be7fdee44226e7e78c4
Leon van der Grient <leon@devtastic.build>
2024-08-16 14:53:22 +0200

    feat: implement content deletion

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -1,11 +1,18 @@
-import { useOne, useTranslate, useUpdate } from "@refinedev/core";
+import { useDelete, useOne, useTranslate, useUpdate } from "@refinedev/core";
 import { useForm } from "@refinedev/react-hook-form";
 import { useCallback, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { PiDotsThreeVerticalBold } from "react-icons/pi";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { ContentFields } from "@/components/content-fields";
 import { Inlines } from "@/components/inlines";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Form } from "@/components/ui/form";
 import { Spinner } from "@/components/ui/spinner";
 import useContentType from "@/hooks/useContentType";
@@ -36,26 +43,29 @@ function Main({
   id?: string | null;
 }) {
   const translate = useTranslate();
+  const navigate = useNavigate();
   const resourceName = contentType.verboseName;
+  const isSingleton = contentType.isSingleton;
   const resourceNamePlural =
     contentType.verboseNamePlural || `${contentType.verboseName}s`;
   const { data, isError, isLoading } = useOne({
     resource: resourceId,
     id,
-    meta: { isSingleton: contentType.isSingleton },
+    meta: { isSingleton },
   });
-  const { mutate } = useUpdate();
+  const { mutate: update } = useUpdate();
+  const { mutateAsync: remove } = useDelete();
 
   useTitle(translate("pages.edit.document_title", { resourceName }));
 
   const form = useForm();
 
   const onSubmit = useCallback((values: any) => {
-    mutate({
+    update({
       resource: resourceId,
       id: id ?? "<SINGLETON>",
       values,
-      meta: { isSingleton: contentType.isSingleton },
+      meta: { isSingleton },
     });
   }, []);
 
@@ -86,21 +96,49 @@ function Main({
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <div className="flex items-center justify-between mb-12">
           <div>
-            <Link
-              to={`/content/${resourceId}`}
-              className="text-sm hover:underline"
-            >
-              {resourceNamePlural}
-            </Link>
+            {!isSingleton && (
+              <Link
+                to={`/content/${resourceId}`}
+                className="text-sm hover:underline"
+              >
+                {resourceNamePlural}
+              </Link>
+            )}
             <h1 className="text-2xl font-semibold truncate">
-              {form.watch("__str__")}
+              {isSingleton ? resourceNamePlural : form.watch("__str__")}
             </h1>
           </div>
-          {contentType.admin?.permissions.change && (
-            <Button loading={form.formState.isSubmitting} type="submit">
-              {translate("common.save")}
-            </Button>
-          )}
+          <div className="flex items-center gap-2">
+            {contentType.admin?.permissions.change && (
+              <Button loading={form.formState.isSubmitting} type="submit">
+                {translate("common.save")}
+              </Button>
+            )}
+            {contentType.admin?.permissions.delete && (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" size="icon">
+                    <PiDotsThreeVerticalBold />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem
+                    className="text-destructive"
+                    onClick={async () => {
+                      if (window.confirm(translate("common.are_you_sure"))) {
+                        try {
+                          await remove({ resource: resourceId, id });
+                          navigate(`/content/${resourceId}`);
+                        } catch (e) {}
+                      }
+                    }}
+                  >
+                    {translate("pages.edit.delete_document", { resourceName })}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+          </div>
         </div>
 
         <div className="bg-white p-8 border rounded">
